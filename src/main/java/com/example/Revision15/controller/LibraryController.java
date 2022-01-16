7e101640cc33a2af5c047032622d044b09b06b41
Jiawenling <jling002@e.ntu.edu.sg>
2022-01-16 11:48:15 +0800

    still has issues extracting author's name

--- src/main/java/com/example/Revision15/controller/LibraryController.java
+++ src/main/java/com/example/Revision15/controller/LibraryController.java
@@ -1,8 +1,11 @@
 package com.example.Revision15.controller;
 
 
+import com.example.Revision15.model.Author;
 import com.example.Revision15.model.Books;
 import com.example.Revision15.service.LibraryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,31 +17,51 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/Library")
 public class LibraryController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LibraryController.class);
 
     @Autowired
     LibraryService libsvc;
 
-    @GetMapping("/savebooks")
+
+    @GetMapping("savebooks")
     public String saveBooks(Model model){
         Books book = new Books();
+        Author author = new Author();
+        model.addAttribute("author", author);
         model.addAttribute("book",book );
-        libsvc.save(book.getId(),book);
+        book.setAuthor(author);
+
         return  "savebook";
     }
 
+    @PostMapping("saved")
+    public String showSavedBooks(Model model, @ModelAttribute Books book, @ModelAttribute Author author){
+        logger.info("Book title: "+ book.getTitle());
+        logger.info("Book id: "+ book.getId());
+        logger.info("Author object: "+ author.name);
+        logger.info("Author: "+ book.getAuthor().name);
+        libsvc.save(book.getId(),book);
+        logger.info("book has been saved");
+        return "savebook";
+    }
 
-    @PostMapping
+
+    @PostMapping("results")
     public String findBooks(@RequestBody MultiValueMap<String, String> form, Model model){
 
         String searchTerm = form.getFirst("searchTerm");
         List<Books> searchResults = new ArrayList<>(libsvc.findBySearchTerm(searchTerm));
-        model.addAttribute("searchResults",searchResults);
+        if(!searchResults.isEmpty()){
+            model.addAttribute("searchResults",searchResults);
+        }
+        model.addAttribute("message","No search results");
+
         return  "index";
     }
 
-    @PostMapping
+    @PostMapping("filtered")
     public String findBooks(Model model, @ModelAttribute List<Books> searchResults,
                             @RequestBody MultiValueMap<String, String> form) {
         Optional<String> pageLimit = Optional.ofNullable(form.getFirst("pageLimit"));
